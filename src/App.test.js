8f354f765769f053bfcdd0cf4284f55ab38f54b5
Marinela <marinelatogoe@gmail.com>
2021-09-11 19:17:15 +0200

    testing implementation

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,39 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('ToDo', () => {
+  const { getByText, getByLabelText } = render(<App />);
+  // after rendering our component
+  getByText('TODO');
+  getByLabelText('Add todo:');
+  getByText('Add #1');
+});
+
+test('add items to list', () => {
+  const { getByText, getByLabelText } = render(<App />);
+  // after rendering our component
+  getByText('TODO');
+  const input = getByLabelText('Add todo:');
+  fireEvent.change(input, { target: { value: 'wash car' } });
+  fireEvent.click(getByText('Add #1'));
+
+  // confirm data
+  getByText('Add #2');
+  getByText('wash car');
+});
+
+// userEvent expresses intent better
+test('user-events allows users to add...', () => {
+  const { getByText, getByLabelText } = render(<App />);
+
+  const input = getByLabelText('Add todo:');
+  const button = getByText('Add #1');
+
+  userEvent.type(input, 'Learn Spanish');
+  userEvent.click(button);
+
+  getByText('Learn Spanish');
+  getByText('Add #2');
 });
